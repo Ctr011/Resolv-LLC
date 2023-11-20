b14aaf737b345a5ce44ef33cdb5fb391c98f9ec0
Godfrey Lozada III <54218111+GodfreyL3@users.noreply.github.com>
2023-11-19 23:09:54 -0800

    Created basic file uploading system.
    When webserver is running, we can now navigate to localhost:8080 to be directed to the file upload page for testing

---
+++ webserver.cpp
@@ -0,0 +1,49 @@
+#include <fstream>
+#include "dependencies/httplib.h"
+
+int main() {
+    httplib::Server svr;
+
+    // Serve static files from the "webpage" directory
+    svr.set_mount_point("/", "./webpage");
+
+    // Serve other static files (JS, CSS, etc.)
+    svr.set_mount_point("/JS", "./webpage/JS");
+    svr.set_mount_point("/CSS", "./webpage/CSS");
+
+    // Serve HTML file
+    svr.Get("/", [](const httplib::Request &, httplib::Response &res) {
+        std::ifstream file("webpage/HTML/fileupload.html");
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        res.set_content(buffer.str(), "text/html");
+    });
+
+    // Just for testing
+    svr.Get("/hi", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+
+    // Endpoint to accept file uploads from the frontend
+    svr.Post("/upload", [&](const httplib::Request &req, httplib::Response &res) {
+        if (req.has_file("file")) {
+            const auto &file = req.get_file_value("file");
+
+            //  @todo: Create new ShipBay Object Here
+
+            // Status code 200: Success
+            res.status = 200;
+            res.set_content("File successfully uploaded", "text/plain");
+        } else {
+            // HTTP status code set to 400: Bad Request
+            res.status = 400;
+            res.set_content("No file uploaded", "text/plain");
+        }
+    });
+
+    // Run web server on localhost (127.0.0.1)
+    std::cout << "Deckware Web Server listening on Port 8080..." << std::endl;
+    svr.listen("localhost", 8080);
+
+    return 0;
+}
