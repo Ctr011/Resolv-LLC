cfec1cd3f5ba09f49a63e01b8ecd80062d5b67ee
Godfrey Lozada III <54218111+GodfreyL3@users.noreply.github.com>
2023-11-29 14:19:12 -0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/godfr/Desktop/CS_179M_Repo
-# Build directory: C:/Users/godfr/Desktop/CS_179M_Repo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test")
